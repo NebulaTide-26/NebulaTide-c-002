4655e13b43648a510236a4f83802c5034e11f34b
starsky-26 <wangshaoyang2602@163.com>
2026-05-30 17:32:14 +0800

    v1.1.0，更改节点复制逻辑，对错误报告进行AI优化等，于2026年5月27日17:32:30

--- 1 模板函数/4 线性表-双向链表.c
+++ 1 模板函数/4 线性表-双向链表.c
@@ -1,14 +1,23 @@
 /* 手动DIY目录：
-DIY_node				
+DIY_data				
 DIY_fillnode_0(k)		
 DIY_fillnode(k)			
 DIY_printnode(k)		（若需调用dlink_printlist时）
 DIY_search(k, x)		（若需调用dlink_search时）
-DIY_copy				（若需调用dlink_copy时）
 
 说明：
 所有涉及intplacecan参数的函数，调用要写PLACE0(x) or PLACE1(x)，代表指针 or 序号
 */
+
+/* 错误说明：
+[Invalid function argument]	非法函数参数
+[Memory exceeded]			MLE
+[Input error]				读取输入失败
+[Illegal operation]			非法操作
+[Other errors]				其他（空链表、位置超范围）
+*/
+
+
 
 #include <stdio.h>
 #include <math.h>
@@ -30,28 +39,29 @@ DIY_copy				（若需调用dlink_copy时）
 #define DIY_search(k, x)	k->num==x
 // 以k作为节点指针；要能直接填写在if()的()里（注意：仅用于简单的节点数据。对于多数据节点，可能要手动修改dlink_search函数！！！！！！）
 
-#define DIY_copy			t2->num = t1->num
-// 将t1节点的所有数据复制给t2节点（t1、t2名称不能变！）
+
 
 #define PLACE0(p) ((intplace){.pr = (p)})
 #define PLACE1(idx) ((intplace){.n = (idx)})
 // 自动填写复合字面量
 
+
+
 /* 数据结构-线性表-双向链表 函数实现
 基本信息类型：intdlink
 节点类型：node
-若同时建立多个顺序表，需建立多个node变量类型 和 多个基本信息变量，并将所有函数的node分别换成对应词（并设定平行的不同函数名称）
+若同时建立多个链表，需建立多个node变量类型 和 多个基本信息变量，并将所有函数的node分别换成对应词（并设定平行的不同函数名称）
 （即：一种基本信息结构体、多种node、多套函数）
 */
 
-static int dlink_error_output = 1;  // 默认打印错误（自定义函数外的static代表仅能在本程序中被访问）
+
+
 // 链表节点结构（具体根据需要填写DIY宏）
 typedef struct Node {
 	DIY_data;
 	struct Node *link1;
 	struct Node *link2;
 } node;
-
 
 // 基本信息结构体
 typedef struct {
@@ -68,31 +78,39 @@ typedef union {
 	int n;
 } intplace;
 
+
+
+static int dlink_error_output = 1;  // 全局变量dlink_error_output：控制是否打印错误信息（默认打印）
+
+
+
 // 1.链表结构控制
-intdlink* dlink_intlist(int n, int sign);
-node* dlink_addnode_head(intdlink *list);
-node* dlink_addnode_tail(intdlink *list);
-node* dlink_addnode(intdlink *list, int target, intplace x, int sign);
-int dlink_deletenode(intdlink *list, int target, intplace x, int sign);
-int dlink_deletelist(intdlink *list);
-intdlink* dlink_append(intdlink* list1, intdlink* list2);
-intdlink* dlink_copy(intdlink* list1);
+intdlink* dlink_intlist(int n, int sign);								// 创建链表
+node* dlink_addnode_head(intdlink *list);								// 向链表头前添加1个空节点
+node* dlink_addnode_tail(intdlink *list);								// 向链表尾后添加1个空节点
+node* dlink_addnode(intdlink *list, int target, intplace x, int sign);	// 向intplace=x的节点的前 or 后添加1个空节点
+int dlink_deletenode(intdlink *list, int target, intplace x, int sign);	// 删除intplace=x的节点 or 其前/后节点
+int dlink_deletelist(intdlink *list);									// 删除全部节点，删除链表信息
+intdlink* dlink_append(intdlink* list1, intdlink* list2);				// 将list2追加到list1后（需保证node类型相同；将删除list2，如需要可提前调用dlink_copy(list2)）
+intdlink* dlink_copy(intdlink* list1);									// 复制list1
 
 // 2.节点位置信息获取&转换
-int dlink_checkplace(intdlink *list, intplace x, int sign);
-node* dlink_search(intdlink* list, int x);
-int dlink_getplace_pr(intdlink *list, node *p);
-node* dlink_getplace_n(intdlink *list, int n);
+int dlink_checkplace(intdlink *list, intplace x, int sign);				// 可视化检查intplace=x的位置信息是否合法
+node* dlink_search(intdlink* list, int x);								// 查找数据（从head开始，查找第一个特定成员值为x的节点）
+int dlink_getplace_pr(intdlink *list, node *p);							// 转换：节点地址->节点序号
+node* dlink_getplace_n(intdlink *list, int n);							// 转换：节点序号->节点地址
 
 // 3.节点信息录入&输出
-void dlink_fillnode_0(intdlink *list, intplace x, int sign);
-int dlink_fillnode(intdlink *list, intplace x, int sign);
-int dlink_printlist(intdlink *list);
+void dlink_fillnode_0(intdlink *list, intplace x, int sign);			// 向intplace=x的节点写入0
+int dlink_fillnode(intdlink *list, intplace x, int sign);				// 向intplace=x的节点录入数据
+int dlink_printlist(intdlink *list);									// 打印全部链表数据
 
-// 4.其他（供其他函数调用的basic函数（不做错误检查）等）
-void dlink_set_error_output(int enable);
-static void dlink_basic_fillnode_0(node *q);
-static int dlink_basic_fillnode(node *q);
+// 4.其他（供其他函数调用的basic函数等）
+void dlink_set_error_output(int enable);								// 控制是否可视化打印错误信息
+static void dlink_basic_fillnode_0(node *q);							// （basic）向某地址为q的节点写入0
+static int dlink_basic_fillnode(node *q);								// （basic）向某地址为q的节点录入数据
+
+//intdlink* dlink_sort(intdlink* list)									// 依照某成员值对链表节点排序（复杂且不急用，待完善）
 
 int main() {
 	
@@ -110,15 +128,15 @@ int main() {
 */
 intdlink* dlink_intlist(int n, int sign_fill) {// sign_fill=1录入数据，sign_fill=0不录入数据
 	if(n<0) {
-		if (dlink_error_output) printf("The number of nodes should >= 0.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The number of nodes should >= 0.\n");
 		return NULL;
 	} else if(sign_fill!=1 && sign_fill!=0) {
-		if (dlink_error_output) printf("The sign to set data should be 0 or 1.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The sign used to set data should be 0 or 1.\n");
 		return NULL;
 	}
-	intdlink *list;
-	if(!(list = (intdlink*)malloc(sizeof(intdlink)))) {// 创建链表信息并判断是否成功
-		if (dlink_error_output) printf("There is not enough space for the new dlinklist.\n");
+	intdlink *list = (intdlink*)malloc(sizeof(intdlink));
+	if(!list) {// 创建链表信息并判断是否成功
+		if (dlink_error_output) printf("[Memory exceeded] There is not enough space for the new dlinklist.\n");
 		return NULL;
 	}
 	list->head=NULL;
@@ -133,7 +151,7 @@ intdlink* dlink_intlist(int n, int sign_fill) {// sign_fill=1录入数据，sign
 	
 	for(int i=1; i<=n; i++) {// 循环创建n个节点
 		if(!(q = (node*)malloc(sizeof(node)))) {// 创建节点并判断是否成功
-			if (dlink_error_output) printf("There is not enough space for the new dlinklist.\n");
+			if (dlink_error_output) printf("[Memory exceeded] There is not enough space for the new dlinklist.\n");
 			dlink_deletelist(list);// 摧毁1~i-1节点
 			return NULL;
 		}
@@ -143,7 +161,7 @@ intdlink* dlink_intlist(int n, int sign_fill) {// sign_fill=1录入数据，sign
 				break;
 			case 1:// 读取数据
 				if(dlink_basic_fillnode(q)<=0) {// 数据不足or输入的数据类型错误
-					if (dlink_error_output) printf("Failed to input data when creating the dlinklist.\n");
+					if (dlink_error_output) printf("[Input error] Failed to input data when creating the dlinklist.\n");
 					dlink_deletelist(list);// 摧毁1~i-1节点
 					free(q);
 					return NULL;
@@ -170,12 +188,12 @@ intdlink* dlink_intlist(int n, int sign_fill) {// sign_fill=1录入数据，sign
 */
 node* dlink_addnode_head(intdlink *list) {
 	if(list==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist pointer is NULL.\n");
 		return NULL;
 	}
-	node *q;
-	if(!(q = (node*)malloc(sizeof(node)))) {// 创建节点并判断是否成功
-		if (dlink_error_output) printf("There is not enough space for the new node.\n");
+	node *q = (node*)malloc(sizeof(node));
+	if(!q) {// 创建节点并判断是否成功
+		if (dlink_error_output) printf("[Memory exceeded] There is not enough space for the new node.\n");
 		return NULL;
 	}
 	dlink_basic_fillnode_0(q);
@@ -199,12 +217,12 @@ node* dlink_addnode_head(intdlink *list) {
 */
 node* dlink_addnode_tail(intdlink *list) {
 	if(list==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist pointer is NULL.\n");
 		return NULL;
 	}
-	node *q;
-	if(!(q = (node*)malloc(sizeof(node)))) {// 创建节点并判断是否成功
-		if (dlink_error_output) printf("There is not enough space for the new node.\n");
+	node *q = (node*)malloc(sizeof(node));
+	if(!q) {// 创建节点并判断是否成功
+		if (dlink_error_output) printf("[Memory exceeded] There is not enough space for the new node.\n");
 		return NULL;
 	}
 	dlink_basic_fillnode_0(q);
@@ -228,11 +246,11 @@ node* dlink_addnode_tail(intdlink *list) {
 */
 node* dlink_addnode(intdlink *list, int target, intplace x, int sign) {// target：-1=前，1=后
 	if(list==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist pointer is NULL.\n");
 		return NULL;
 	}
 	if(target!=-1 && target!=1) {
-		if (dlink_error_output) printf("The \"target\" used to determine direction should be -1 or 1.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The \"target\" used to determine direction should be -1 or 1.\n");
 		return NULL;
 	}
 	if(dlink_checkplace(list, x, sign)==-1) {
@@ -250,9 +268,9 @@ node* dlink_addnode(intdlink *list, int target, intplace x, int sign) {// target
 			break;
 	}
 	// 此时：链表size一定>=2，且新节点不插在头尾
-	node *q;
-	if(!(q = (node*)malloc(sizeof(node)))) {// 创建节点并判断是否成功
-		if (dlink_error_output) printf("There is not enough space for the new node.\n");
+	node *q = (node*)malloc(sizeof(node));
+	if(!q) {// 创建节点并判断是否成功
+		if (dlink_error_output) printf("[Memory exceeded] There is not enough space for the new node.\n");
 		return NULL;
 	}
 	dlink_basic_fillnode_0(q);
@@ -282,7 +300,7 @@ int dlink_deletenode(intdlink *list, int target, intplace x, int sign) {// targe
 		return -1;
 	}
 	if(target!=-1 && target!=0 && target!=1) {
-		if (dlink_error_output) printf("The \"target\" used to determine direction should be -1 , 0 or 1.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The \"target\" used to determine direction should be -1 , 0 or 1.\n");
 		return -1;
 	}
 	
@@ -318,13 +336,13 @@ int dlink_deletenode(intdlink *list, int target, intplace x, int sign) {// targe
 			break;
 		case -1:// 删除前一节点
 			if(x.pr==list->head) {
-				if (dlink_error_output) printf("Cannot delete the preceding node.(already at boundary)\n");
+				if (dlink_error_output) printf("[Illegal operation] Cannot delete the preceding node.(already at boundary)\n");
 				return -1;
 			}
 			return dlink_deletenode(list, 0, PLACE0(x.pr->link1), 0);
 		case 1:// 删除后一节点
 			if(x.pr==list->tail) {
-				if (dlink_error_output) printf("Cannot delete the following node.(already at boundary)\n");
+				if (dlink_error_output) printf("[Illegal operation] Cannot delete the following node.(already at boundary)\n");
 				return -1;
 			}
 			return dlink_deletenode(list, 0, PLACE0(x.pr->link2), 0);
@@ -337,7 +355,7 @@ int dlink_deletenode(intdlink *list, int target, intplace x, int sign) {// targe
 */
 int dlink_deletelist(intdlink *list) {
 	if(list==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist pointer is NULL.\n");
 		return -1;
 	}
 	for(node *t=list->head, *p; t!=NULL;) {
@@ -354,13 +372,13 @@ int dlink_deletelist(intdlink *list) {
 */
 intdlink* dlink_append(intdlink* list1, intdlink* list2) {
 	if(list1==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist 1.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist 1 pointer is NULL.\n");
 		return NULL;
 	} else if(list2==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist 2.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist 2 pointer is NULL.\n");
 		return NULL;
 	} else if(list2==list1) {
-		if (dlink_error_output) printf("Can't append a list to itself.\n");
+		if (dlink_error_output) printf("[Illegal operation] Cannot append a list to itself.\n");
 		return NULL;
 	}
 	// 单独处理空表问题
@@ -388,18 +406,27 @@ intdlink* dlink_append(intdlink* list1, intdlink* list2) {
 
 /* 复制list1
    返回值：复制出来的链表的信息结构体指针（失败返回NULL）
+   
+   注意：节点data部分为浅拷贝，若data区存在指针，可能会导致double-free，调用时小心
 */
 intdlink* dlink_copy(intdlink* list1) {
 	if(list1==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist pointer is NULL.\n");
 		return NULL;
 	}
 	intdlink *list2 = dlink_intlist(list1->size, 0);
+	if(!list2) {
+		return NULL;
+	}
 	if(list1->size==0) {
 		return list2;
 	}
 	for(node *t1=list1->head, *t2=list2->head; t1!=NULL; t1=t1->link2, t2=t2->link2) {
-		DIY_copy;
+		node *link1 = t2->link1;
+		node *link2 = t2->link2;
+		*t2 = *t1;
+		t2->link1 = link1;
+		t2->link2 = link2;
 	}
 	return list2;
 }
@@ -415,15 +442,15 @@ intdlink* dlink_copy(intdlink* list1) {
 */
 int dlink_checkplace(intdlink *list, intplace x, int sign) {
 	if(sign!=0 && sign!=1) {
-		if (dlink_error_output) printf("The sign to translate \"intplace\" should be 0 or 1.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The sign used to translate \"intplace\" should be 0 or 1.\n");
 		return -1;
 	}
 	if(list==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist pointer is NULL.\n");
 		return -1;
 	}
 	if(list->size==0) {
-		if (dlink_error_output) printf("The target dlinklist is empty.\n");
+		if (dlink_error_output) printf("[Other errors] The target dlinklist is empty.\n");
 		return -1;
 	}
 	
@@ -431,7 +458,7 @@ int dlink_checkplace(intdlink *list, intplace x, int sign) {
 		case 0:{// 地址
 			node *pr = x.pr;
 			if(pr==NULL) {
-				if (dlink_error_output) printf("Invalid position.(the position should not be NULL)\n");
+				if (dlink_error_output) printf("[Invalid function argument] The position should not be NULL.\n");
 				return -1;
 			}
 			return 1;
@@ -440,11 +467,11 @@ int dlink_checkplace(intdlink *list, intplace x, int sign) {
 		case 1:{// 序号
 			int n = x.n;
 			if(n<=0) {
-				if (dlink_error_output) printf("Invalid position.(the node number should >= 1)\n");
+				if (dlink_error_output) printf("[Invalid function argument] The node number should >= 1.\n");
 				return -1;
 			}
 			if(n>list->size) {
-				if (dlink_error_output) printf("Can't find the target node.(longer than the list's size)\n");
+				if (dlink_error_output) printf("[Other errors] Cannot find the target node.(longer than the list's size)\n");
 				return -1;
 			}
 			return 1;
@@ -461,7 +488,7 @@ int dlink_checkplace(intdlink *list, intplace x, int sign) {
 */
 node* dlink_search(intdlink* list, int x) {
 	if(list==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist pointer is NULL.\n");
 		return NULL;
 	}
 	if(list->size==0) {
@@ -488,7 +515,7 @@ int dlink_getplace_pr(intdlink *list, node *p) {
 			return count;
 		}
 	}
-	if (dlink_error_output) printf("Invalid position.(the position isn't in the list)\n");
+	if (dlink_error_output) printf("[Invalid function argument] The position isn't in the list.\n");
 	return -1;
 }
 
@@ -533,7 +560,12 @@ int dlink_fillnode(intdlink *list, intplace x, int sign) {
 	if(sign) {// 统一翻译成x.pr
 		x.pr = dlink_getplace_n(list, x.n);
 	}
-	return dlink_basic_fillnode(x.pr);
+	int t;
+	if((t = dlink_basic_fillnode(x.pr))<=0) {// 数据不足or输入的数据类型错误
+		if (dlink_error_output) printf("[Input error] Failed to input data.\n");
+		return -1;
+	}
+	return t;
 }
 
 /* 打印全部链表数据
@@ -541,13 +573,13 @@ int dlink_fillnode(intdlink *list, intplace x, int sign) {
 */
 int dlink_printlist(intdlink *list) {
 	if(list==NULL) {
-		if (dlink_error_output) printf("Can't find the target dlinklist.\n");
+		if (dlink_error_output) printf("[Invalid function argument] The target dlinklist pointer is NULL.\n");
 		return -1;
 	}
 	node *t=list->head;
 	int count=0;
 	if(t==NULL) {
-		if (dlink_error_output) printf("The dlinklist is empty.\n");
+		if (dlink_error_output) printf("The target dlinklist is empty.\n");
 		return -1;
 	}
 	printf("Here are contents of the dlinklist now:\n");
@@ -561,7 +593,8 @@ int dlink_printlist(intdlink *list) {
 
 
 
-// 4.其他（供其他函数调用的basic函数（不做错误检查）等）
+// 4.其他（供其他函数调用的basic函数等）
+// 注：basic函数不做可靠的错误检查，仅做部分错误反馈，尽量不要手动调用
 /* 控制是否可视化打印错误信息
 */
 void dlink_set_error_output(int enable) {// 1→打印；0→不打印
